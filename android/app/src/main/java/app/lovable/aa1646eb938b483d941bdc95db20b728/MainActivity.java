b6283329189a31470cf49bd8663bc1b8d804ac82
annklin <71550518+annklin@users.noreply.github.com>
2026-03-14 12:54:34 -0300

    Update MainActivity.java

--- android/app/src/main/java/app/lovable/aa1646eb938b483d941bdc95db20b728/MainActivity.java
+++ android/app/src/main/java/app/lovable/aa1646eb938b483d941bdc95db20b728/MainActivity.java
@@ -1,5 +1,16 @@
 package app.lovable.aa1646eb938b483d941bdc95db20b728;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    MobileAds.initialize(this);
+  }
+
+}
