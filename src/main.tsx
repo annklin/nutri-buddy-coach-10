4e092bf50f83236aad773755de06f25eae027f3a
annklin <71550518+annklin@users.noreply.github.com>
2026-03-15 01:14:27 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+import { iniciarAds } from "./services/admob"
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { applyTheme } from "./lib/theme";
 
 applyTheme();
+iniciarAds();
 
 createRoot(document.getElementById("root")!).render(<App />);
