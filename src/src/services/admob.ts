7b070e564010cafb07a48a2254f89e81dff5f94e
annklin <71550518+annklin@users.noreply.github.com>
2026-03-15 05:29:52 -0300

    Update admob.ts
    Test

--- src/src/services/admob.ts
+++ src/src/services/admob.ts
@@ -1,25 +1,35 @@
-import { AdMob } from "@capacitor-community/admob"
+import { AdMob } from "@capacitor-community/admob";
 
 export async function iniciarAds() {
-  await AdMob.initialize()
+  await AdMob.initialize();
 }
 
-export async function anuncioMacros() {
+export async function showRewardedAd() {
+  try {
+    await AdMob.prepareRewardVideoAd({
+      adId: "ca-app-pub-3940256099942544/5224354917" // teste
+    });
 
-  await AdMob.prepareInterstitial({
-    adId: "ca-app-pub-9088121551421320/8743324289"
-  })
+    await AdMob.showRewardVideoAd();
 
-  await AdMob.showInterstitial()
-
+    return true;
+  } catch (error) {
+    console.log("Erro rewarded", error);
+    return false;
+  }
 }
 
-export async function anuncio5Alimentos() {
+export async function showInterstitialAd() {
+  try {
+    await AdMob.prepareInterstitial({
+      adId: "ca-app-pub-3940256099942544/1033173712" // teste
+    });
 
-  await AdMob.prepareInterstitial({
-    adId: "ca-app-pub-9088121551421320/1581297367"
-  })
+    await AdMob.showInterstitial();
 
-  await AdMob.showInterstitial()
-
+    return true;
+  } catch (error) {
+    console.log("Erro interstitial", error);
+    return false;
+  }
 }
