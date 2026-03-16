fed3203d4b47e86a9c0d1bcb66b1f520c3433401
annklin <71550518+annklin@users.noreply.github.com>
2026-03-16 01:10:07 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,14 @@
 
 
-import { iniciarAds } from "./src/services/admob";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { applyTheme } from "./lib/theme";
 
-// Aplicar tema
-applyTheme();
-
-// Iniciar anúncios
-iniciarAds();
-
-// ✅ Ativar Premium automaticamente se vier de Stripe
+// Ativa Premium se URL tiver ?premium=true
 const params = new URLSearchParams(window.location.search);
 if (params.get("premium") === "true") {
   localStorage.setItem("premium_user", "true");
   window.history.replaceState({}, document.title, "/"); // remove ?premium=true da URL
 }
 
-// Criar root do React
 createRoot(document.getElementById("root")!).render(<App />);
