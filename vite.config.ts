21bd181c75880f1e07578d985ce50cd1d5648faa
annklin <71550518+annklin@users.noreply.github.com>
2026-03-16 22:50:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-    server: {
+  base: "./",
+
+  server: {
     host: "::",
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
+
   plugins: [react()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
