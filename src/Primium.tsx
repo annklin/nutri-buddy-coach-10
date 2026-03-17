d1edc9fc572d6e18c39d7343827e76198bc13506
annklin <71550518+annklin@users.noreply.github.com>
2026-03-16 22:42:19 -0300

    Update Primium.tsx

--- src/Primium.tsx
+++ src/Primium.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface PremiumProps {
   toggleTheme: () => void;
